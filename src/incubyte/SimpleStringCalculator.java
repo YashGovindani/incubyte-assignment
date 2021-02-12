1419f349dddb25f0babd0da69dfea5d8a09f2b73
YashGovindani <yashgovindani222@gmail.com>
2021-02-12 17:10:11 +0530

    stage 2 work compiled successfully

--- src/incubyte/SimpleStringCalculator.java
+++ src/incubyte/SimpleStringCalculator.java
@@ -2,13 +2,37 @@ package incubyte;
 
 public class SimpleStringCalculator
 {
-public static int Add(String args)
+static private boolean hasDelimeter;
+private static String getDelimeter(String args) throws IllegalArgumentException
+{
+int endIndex;
+hasDelimeter=false;
+endIndex=args.indexOf("]\n");
+if(args.startsWith("//[")==false && endIndex!=-1)
+ throw new IllegalArgumentException("Invalid string : "+args);
+if(args.startsWith("//[") && endIndex==-1)
+ throw new IllegalArgumentException("Invalid string : "+args);
+if(args.startsWith("//[")==false && endIndex==-1) return ",";
+hasDelimeter=true;
+return args.substring(3,endIndex);
+}
+private static boolean isValid(String args,String delimeter)
+{
+if(args.indexOf("\n"+delimeter)!=-1 || args.indexOf(delimeter+"\n")!=-1) return false;
+return true;
+}
+public static int Add(String args) throws IllegalArgumentException
 {
 int sum;
+String delimeter;
 if(args.trim().isEmpty()) return 0;
-String nums[]=args.trim().split(",");
+delimeter=getDelimeter(args);
+if(!isValid(args,delimeter)) throw new IllegalArgumentException("Invalid string : "+args);
+String nums[];
+if(!hasDelimeter) nums=args.replaceAll("\n",delimeter).trim().split(delimeter);
+else nums=args.substring(args.indexOf("]\n")+2).replaceAll("\n",delimeter).trim().split(delimeter);
 sum=0;
-for(String num:nums) sum+=Integer.parseInt(num);
+for(String num:nums) sum+=Integer.parseInt(num.trim());
 return sum;
 }
 }
