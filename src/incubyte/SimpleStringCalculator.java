578339fcd0e6140340e5b7f28b3a90ef305e02b5
YashGovindani <yashgovindani222@gmail.com>
2021-02-12 19:33:31 +0530

    Assignment Completed

--- src/incubyte/SimpleStringCalculator.java
+++ src/incubyte/SimpleStringCalculator.java
@@ -1,5 +1,5 @@
 package incubyte;
-
+import java.util.*;
 public class SimpleStringCalculator
 {
 static private boolean hasDelimeter;
@@ -16,6 +16,31 @@ if(args.startsWith("//[")==false && endIndex==-1) return ",";
 hasDelimeter=true;
 return args.substring(3,endIndex);
 }
+private static List<String> getDelimeters(String args) throws IllegalArgumentException
+{
+int startIndex,endIndex;
+List<String> delimeters;
+hasDelimeter=false;
+endIndex=args.indexOf("]");
+delimeters=new ArrayList<String>();
+if(args.startsWith("//[")==false && endIndex!=-1)
+ throw new IllegalArgumentException("Invalid string : "+args);
+if(args.startsWith("//[") && endIndex==-1)
+ throw new IllegalArgumentException("Invalid string : "+args);
+if(args.startsWith("//[")==false && endIndex==-1) delimeters.add(",");
+if(delimeters.size()==1) return delimeters;
+startIndex=2;
+while(true)
+{
+delimeters.add(args.substring(startIndex+1,endIndex));
+startIndex=endIndex+1;
+if(args.charAt(startIndex)!='[') break;
+endIndex=args.indexOf("]",startIndex);
+if(endIndex==-1) throw new IllegalArgumentException("Invalid string : "+args);
+}
+hasDelimeter=true;
+return delimeters;
+}
 private static boolean isValid(String args,String delimeter)
 {
 if(args.indexOf("\n"+delimeter)!=-1 || args.indexOf(delimeter+"\n")!=-1) return false;
@@ -24,18 +49,28 @@ return true;
 public static int Add(String args) throws IllegalArgumentException
 {
 int sum,i;
-String delimeter,exceptionString;
+//String delimeter,
+String exceptionString;
+List<String> delimeters;
 if(args.trim().isEmpty()) return 0;
-delimeter=getDelimeter(args);
-if(!isValid(args,delimeter)) throw new IllegalArgumentException("Invalid string : "+args);
+//delimeter=getDelimeter(args);
+delimeters=getDelimeters(args);
+//if(!isValid(args,delimeter)) throw new IllegalArgumentException("Invalid string : "+args);
 String nums[];
-if(!hasDelimeter) nums=args.replaceAll("\n",delimeter).trim().split(delimeter);
-else nums=args.substring(args.indexOf("]\n")+2).replaceAll("\n",delimeter).trim().split(delimeter);
+if(!hasDelimeter) nums=args.replaceAll("\n",",").trim().split(",");
+//else nums=args.substring(args.indexOf("]\n")+2).replaceAll("\n",delimeter).trim().split(delimeter);
+else
+{
+for(String delimeter:delimeters) args=args.replaceAll(delimeter,",");
+args=args.substring(args.indexOf("]\n")+2).replaceAll("\n",",");
+nums=args.trim().split(",");
+}
 sum=0;
 exceptionString="Negative number(s) : ";
 for(String num:nums)
 {
 i=Integer.parseInt(num.trim());
+if(i>1000) continue;
 if(i<0) exceptionString+=i+",";
 sum+=Integer.parseInt(num.trim());
 }
---
+++ test/stage4.java
@@ -0,0 +1,39 @@
+import incubyte.SimpleStringCalculator;
+
+class stage4Test
+{
+public static void main(String gs[])
+{
+int sum;
+String args;
+System.out.println("For stage 3 Test Cases :");
+try
+{
+args="//[;]\n10;2;3\n11;123\n12;854;57643;2356;-12343";
+System.out.print("Case 1 : passed \""+args+"\", sum=");
+sum=SimpleStringCalculator.Add(args);
+System.out.println(sum);
+}catch(IllegalArgumentException iae){System.out.println(iae.getMessage());}
+try
+{
+args="[]\n10;2;3;11;123;12;854;57643;2356;-12343";
+System.out.print("Case 2 : passed \""+args+"\", sum=");
+sum=SimpleStringCalculator.Add(args);
+System.out.println(sum);
+}catch(IllegalArgumentException iae){System.out.println(iae.getMessage());}
+try
+{
+args="//[xyz]\n10xyz2xyz\nxyz11xyz123xyz12xyz854xyz57643xyz2356xyz-12343";
+System.out.print("Case 3 : passed \""+args+"\", sum=");
+sum=SimpleStringCalculator.Add(args);
+System.out.println(sum);
+}catch(IllegalArgumentException iae){System.out.println(iae.getMessage());}
+try
+{
+args="//[xyz][;]\n10xyz2;4578\n485xyz11xyz123;12xyz854xyz57643;2356xyz12343";
+System.out.print("Case 4 : passed \""+args+"\", sum=");
+sum=SimpleStringCalculator.Add(args);
+System.out.println(sum);
+}catch(IllegalArgumentException iae){System.out.println(iae);}
+}
+}
